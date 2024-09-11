55c1e0f83b85aed87be10f54ab9f85b42b381157
SlavaKozodoi <152405589+SlavaKozodoi@users.noreply.github.com>
2024-09-11 15:25:05 +0300

    lab2 done

---
+++ Lab_2_Kozodoi.py
@@ -0,0 +1,23 @@
+
+#1
+print("Programming", "Essential", "in",sep="***",end="...")
+print("Python")
+
+#2
+print("    *\n   * *\n  *   *\n *    *\n***   ***\n  *   *\n  *   *\n  *****")
+
+#3
+print("I'm student")
+
+#4
+print("\"I'm\" \n\"\"learning\"\" \n\"\"\"Python\"\"\"")
+
+#5
+o_number='500'
+d_number=int(o_number,8)
+print(d_number)
+
+#6
+o_number='777'
+d_number=int(o_number,16)
+print(d_number)
