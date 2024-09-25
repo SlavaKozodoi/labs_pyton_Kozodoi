2004aa38efe2e8f1f85ebd1d01684f6a35cc6993
SlavaKozodoi <152405589+SlavaKozodoi@users.noreply.github.com>
2024-09-25 19:44:14 +0300

    lab4 done

--- Lab_4_Kozodoi.py
+++ Lab_4_Kozodoi.py
@@ -42,6 +42,89 @@ else:
     print(year, "Not within the Gregorian calendar period.")
 
 #6
+secret_number = 777
+print(
+"""
++================================+
+| Welcome to my game, muggle!    |
+| Enter an integer number        |
+| and guess what number I've     |
+| picked for you.                |
+| So, what is the secret number? |
++================================+
+""")
+enterWord = ""
+while(enterWord != secret_number):
+    enterWord = int(input("Enter a number: "))
+    print("Ха-ха! Ви застрягли у моїй петлі!")
+else:
+    print("Молодець, магле! Тепер ти вільний")
+
+#7
+import time
+
+for it in range(0,5):
+    print(it+1,"Mississippi")
+    time.sleep(1)
+
+#8
+while True:
+    if(input("8 Input a word ")=="chupacabra"):
+        break
+print("You've successfully left the loop.")
+
+#9
+user_word = input("Введіть слово: ")
+user_word = user_word.upper()
+vowels = "AEIOU"
+
+for letter in user_word:
+    if letter in vowels:
+        continue
+    print(letter)
+
+#10
+word_without_vowels = ""
+user_word = input("Введіть слово: ")
+user_word = user_word.upper()
+vowels = "AEIOU"
+
+for letter in user_word:
+    if letter in vowels:
+        continue
+    word_without_vowels += letter
+
+print("Слово без голосних:", word_without_vowels)
+
+#11
+blocks = int(input("Введіть кількість блоків: "))
+height = 0
+required_blocks = 1
+
+while blocks >= required_blocks:
+    blocks -= required_blocks
+    height += 1
+    required_blocks += 1
+print("Максимальна висота піраміди:", height)
+
+#12
+c0 = int(input("Введіть натуральне число: "))
+steps = 0
+
+while c0 != 1:
+    print(c0)
+    if c0 % 2 == 0:
+        c0 = c0 // 2
+    else:
+        c0 = 3 * c0 + 1
+    steps += 1
+
+print(c0)
+print("Кількість кроків до досягнення 1:", steps)
+
+
+
+
 
 
 
